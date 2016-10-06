eb0b9def70fa65ff7b492e2326bbfbefa2fe03af
dogvincnet1982 <dogvincent@hotmail.com>
2016-10-06 08:28:42 +0800

    加上註解

--- TDD_Homework/Day1Homework/Day1Homework/MathService.cs
+++ TDD_Homework/Day1Homework/Day1Homework/MathService.cs
@@ -9,12 +9,21 @@ namespace Day1Homework
 {
     public class MathService
     {
+        /// <summary>
+        /// 依照給的List、筆數與屬性名稱回傳以筆數為一個群組的屬性質加總
+        /// </summary>
+        /// <typeparam name="T">List的型別</typeparam>
+        /// <param name="objectList">要計算的List</param>
+        /// <param name="count">以N筆為一個群組</param>
+        /// <param name="propertyName">要加總的屬性名稱</param>
+        /// <returns>加總的結果</returns>
         public List<int> GetSumPropertyGroupByCount<T>(List<T> objectList, int count, string propertyName)
         {
             List<int> returnValue = new List<int>();
             if (objectList != null && objectList.Count > 0)
             {
                 Type type = objectList.First().GetType();
+                PropertyInfo propertyInfo = type.GetProperty(propertyName);
                 int nowIndex = 0;
                 int tempSum = 0;
                 foreach (T obj in objectList)
@@ -23,7 +32,6 @@ namespace Day1Homework
                      * 預設狀態為內部使用Dll
                      * 以下若轉型或取得屬性失敗皆直接throw exception不處理
                     */
-                    PropertyInfo propertyInfo = type.GetProperty(propertyName);
                     tempSum += (int)propertyInfo.GetValue((T)obj);
                     nowIndex++;
                     if (nowIndex >= count)
