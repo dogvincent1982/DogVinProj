557a49be3cb2456a7e8276e00b086660a4b4b3ac
dogvincnet1982 <dogvincent@hotmail.com>
2016-10-23 02:21:00 +0800

    second test case

--- TDD_Homework/Day2Homework/Day2Homework/ShoppingCartService.cs
+++ TDD_Homework/Day2Homework/Day2Homework/ShoppingCartService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Day2Homework
 {
@@ -7,13 +8,35 @@ namespace Day2Homework
     {
         public int CalculateTotalAmount(List<BookInfo> buyBookList)
         {
+            List<int> bookHarryPotterEpisodeCountList = new List<int>() { 0, 0, 0, 0, 0 };
             int returnValue = 0;
+            float discount = 1;
             foreach (BookInfo bookInfo in buyBookList)
             {
                 if (bookInfo.BookName == "Harry Potter I")
                 {
-                    returnValue += 100;
+                    bookHarryPotterEpisodeCountList[0]++;
                 }
+                else if (bookInfo.BookName == "Harry Potter II")
+                {
+                    bookHarryPotterEpisodeCountList[1]++;
+                }
+            }
+            for (int i = bookHarryPotterEpisodeCountList.Max(); i > 0 ; i--)
+            {
+                int bookCount = bookHarryPotterEpisodeCountList.Where(item => item / i == 1).Count();
+                switch (bookCount)
+                {
+                    case 1:
+                        discount = 1;
+                        break;
+                    case 2:
+                        discount = 0.95f;
+                        break;
+                    default:
+                        continue;
+                }
+                returnValue += Convert.ToInt32((int)bookCount * discount * 100);
             }
             return returnValue;
         }
--- TDD_Homework/Day2Homework/UnitTestDay2Homework/UnitTest1.cs
+++ TDD_Homework/Day2Homework/UnitTestDay2Homework/UnitTest1.cs
@@ -21,5 +21,20 @@ namespace UnitTestDay2Homework
             int expected = 100;
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
+        public void TestShoppingCart_BuyFirstAndSecondEpisode_Return190()
+        {
+            ShoppingCartService shoppingCartService = new ShoppingCartService();
+
+            List<BookInfo> buyBookList = new List<BookInfo>()
+            {
+                new BookInfo() { BookName = "Harry Potter I",Amount=1},
+                new BookInfo() { BookName = "Harry Potter II",Amount=1}
+            };
+            int actual = shoppingCartService.CalculateTotalAmount(buyBookList);
+            int expected = 190;
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
